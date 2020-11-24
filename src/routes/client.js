b0ef4ea0607c13f3686de520cedbd5d46f9e8b20
dger804 <diego804@gmail.com>
2020-11-24 08:26:41 -0500

    Client routes creation

---
+++ src/routes/client.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const clientController = require('../controllers/client.controller')
+
+router.route('/').post(clientController.create);
+router.route('/:clientId').post(clientController.show);
+router.route('/:clientId').put(clientController.update);
+router.route('/:clientId').delete(clientController.destroy);
+
+module.exports = router;
