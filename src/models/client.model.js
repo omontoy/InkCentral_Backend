6deb90570984fb36207d2667e1a7c622e4673b9b
dger804 <diego804@gmail.com>
2020-11-24 07:34:09 -0500

    Add validator to client model

--- src/models/client.model.js
+++ src/models/client.model.js
@@ -1,10 +1,21 @@
-const { model, Schema } = require('mongoose')
+const { model, Schema, models } = require('mongoose')
 
 const clientSchema = new Schema ({
   name: String,
   email: {
     type: String,
     required: true,
+    match: [emailRegexp, 'Invalid Email'],
+    validate: [
+      {
+        validator(value){
+          return models.Client.findOne( { email: value } )
+            .then(artist => !Client)
+            .catch(() => false)
+        },
+        message: "Email already exists"
+      }
+    ]
   },
   password: {
     type: String,
