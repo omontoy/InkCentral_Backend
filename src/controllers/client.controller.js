de22b4f85c76cb09c19f188f3d34f2eca78cc2ca
dger804 <diego804@gmail.com>
2020-11-25 18:53:28 -0500

    Fix functional backend client

--- src/controllers/client.controller.js
+++ src/controllers/client.controller.js
@@ -1,15 +1,49 @@
 const bcrypt = require('bcrypt');
+const  jwt = require('jsonwebtoken');
 const Client = require('../models/client.model');
 
 module.exports = {
+  async list(req, res){
+    try {
+      const clients = await Client.find();
+      res.status(200).json( { message: 'Clients found', data: clients } )
+    } catch (err){
+      res.status(400).json( { message: err.message } )
+    };
+  },
   async create(req, res){
     try {
       const { email, password } = req.body;
+      if(password.lenght < 4 || password.lenght > 8){
+        throw new Error( 'Your password must be between 4 and 8 characters' )
+      }
       const encPassword = await bcrypt.hash(password, 8);
-      const client = await Client.create( { email, password: encPassword })
-      res.status(201).json({ message: 'Client Created', data: client })
+      const client = await Client.create( { email, password: encPassword } )
+      res.status(201).json( { message: 'Client Created', data: client } )
     } catch (err){
-      res.status(400).json(err.errors.email.message)
+      res.status(400).json( { message: err.message })
+    };
+  },
+  async login(req, res){
+    try{
+      const { email, password } = req.body;
+      const client = await Client.findOne({ email });
+      console.log(client)
+      if( !client ){
+        throw new Error( 'Invalid email or password' )
+      }
+      const isValid = await bcrypt.compare(password, client.password);
+      if( !isValid ){
+        throw new Error( 'Invalid email or password' )
+      }
+      const token = jwt.sign(
+        { id: client._id },
+        process.env.SECRET,
+        { expiresIn: 60 * 60 * 24 }
+      )
+      res.status(200).json( { token } )
+    } catch (err){
+        res.status(400).json( { message: err.message } )
     };
   },
   async show(req, res){
@@ -19,7 +53,7 @@ module.exports = {
       if( !client ){
         throw new Error( 'Invalid ID' )
       }
-      res.status(200).json( {message: "Encontrado", data: client} );      
+      res.status(200).json( { message: "Encontrado", data: client } );      
     } catch (err){
       res.status(400).json( err.message );
     }
--- src/routes/client.js
+++ src/routes/client.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const clientController = require('../controllers/client.controller')
 
+router.route('/').get(clientController.list);
 router.route('/').post(clientController.create);
+router.route('/login').post(clientController.login);
 router.route('/:clientId').get(clientController.show);
 router.route('/:clientId').put(clientController.update);
 router.route('/:clientId').delete(clientController.destroy);
