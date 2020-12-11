6ad3ba73e2e6019878a7db62be002b5381689093
Orlando Montoya <omb0320@gmail.com>
2020-12-11 00:41:00 -0500

    Modify show and update client routes

--- src/routes/client.js
+++ src/routes/client.js
@@ -2,15 +2,12 @@ const router = require('express').Router();
 const clientController = require('../controllers/client.controller')
 const { auth } = require('../utils/auth')
 
-router.route('/:clientId').get(auth, clientController.show);
-router.route('/').put(auth, clientController.update);
+router.route('/:profile').get(auth, clientController.show);
+router.route('/profile').put(auth, clientController.update);
 router.route('/:clientId').delete(auth, clientController.destroy);
 
-router.route('/').get(clientController.list);
 router.route('/').post(clientController.create);
 router.route('/login').post(clientController.login);
-
-
 
 
 module.exports = router;
